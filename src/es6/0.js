f89339eca56a7f47a96f05eeaf731e89f441fbd3
Fabio H. Mancipe <fabiohm003@yahoo.es>
2024-04-19 06:34:37 -0500

    0

---
+++ src/es6/0.js
@@ -0,0 +1,14 @@
+var n1 = 'test';
+
+console.log(n1);
+
+
+/////////
+
+
+let valor = (val1, val2, val3) => {
+return `paso: ${val1} ${val2} ${val3}`;
+}
+
+
+console.log(valor(1, 2, 3));
---
+++ src/es6/2-str.js
@@ -0,0 +1,7 @@
+let v1 = 'aaa';
+let v2 = 'bbb';
+
+
+let r = `${v1} ${v2}`;
+
+console.log(r);
